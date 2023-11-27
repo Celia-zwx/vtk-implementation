88d548f98f619b31b508b1f4a566abb3b17be379
unknown <wzhang488@gatech.edu>
2023-11-27 18:36:18 +0800

    a

--- test/double T cells/doubleTcells.cxx
+++ test/double T cells/doubleTcells.cxx
@@ -1,7 +1,7 @@
 /**
  * @brief this file works for generating double T cells in vtk
  * @author zwx3991@gmail.com
- * @date 2023-11-08
+ * @date 2023-11-27
  */
 #include <vtkAutoInit.h>
 #include <vtkActor.h>
@@ -9,6 +9,7 @@
 #include <vtkDataSetMapper.h>
 #include <vtkLinearExtrusionFilter.h>
 #include <vtkNamedColors.h>
+#include <vtkPolyDataMapper.h>
 #include <vtkNew.h>
 #include <vtkProperty.h>
 #include <vtkRenderWindow.h>
@@ -16,6 +17,7 @@
 #include <vtkRenderer.h>
 #include <vtkTriangleFilter.h>
 #include <vtkVectorText.h>
+#include <vtkStripper.h>
 
 int main(int, char *[])
 {
@@ -23,15 +25,10 @@ int main(int, char *[])
   // Create a renderer, render window, and interactor
   vtkNew<vtkRenderer> renderer;
   vtkNew<vtkRenderWindow> renderWindow;
-  renderWindow->AddRenderer(renderer);
-  renderWindow->SetWindowName("DoubleTCell");
-  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
-  renderWindowInteractor->SetRenderWindow(renderWindow);
-  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
 
-  // 创建一个vtkPoints对象，并添加一些点
+  // create a vtk object and insert some points
   vtkNew<vtkPoints> points;
-  points->InsertNextPoint(0, 0, 0); // 添加点的坐标 (x, y, z)
+  points->InsertNextPoint(0, 0, 0); //  (x, y, z)
   points->InsertNextPoint(0, 1, 0);
   points->InsertNextPoint(3, 1, 0);
   points->InsertNextPoint(3, 0, 0);
@@ -45,51 +42,66 @@ int main(int, char *[])
   points->InsertNextPoint(1, 0, 0);
 
 
-  // 创建一个vtkCellArray对象来定义多边形
-  vtkNew<vtkCellArray> polygons;
+// Create a vtkCellArray object to define polygons
+vtkNew<vtkCellArray> polygons;
 
-  // 创建一个包含多边形顶点索引的vtkIdList对象
-  vtkNew<vtkIdList> pointIds;
-  pointIds->SetNumberOfIds(12); 
+// Create a vtkIdList object containing indices of polygon vertices
+vtkNew<vtkIdList> pointIds;
+pointIds->SetNumberOfIds(12);
 
-  // 设置多边形的顶点索引
-  for (int i = 0; i < 12; i++)
-  {
-    pointIds->SetId(i, i);
-  }
+// Set indices of vertices for the polygon
+for (int i = 12; i > 0; i--)
+{
+  pointIds->SetId(i, i);
+}
 
-  // 将多边形添加到vtkCellArray中
-  polygons->InsertNextCell(pointIds);
+// Add the polygon to the vtkCellArray
+polygons->InsertNextCell(pointIds);
 
-  // 创建一个vtkPolyData对象
-  vtkNew<vtkPolyData> polyData;
+// Create a vtkPolyData object
+vtkNew<vtkPolyData> polyData;
 
-  // 将vtkPoints和vtkCellArray添加到vtkPolyData中
-  polyData->SetPoints(points);
-  polyData->SetPolys(polygons);
+// Add vtkPoints and vtkCellArray to vtkPolyData
+polyData->SetPoints(points);
+polyData->SetPolys(polygons);
 
-  // Apply linear extrusion
+
+  //Apply linear extrusion
   vtkNew<vtkLinearExtrusionFilter> extrude;
   extrude->SetInputData(polyData);
-  extrude->SetExtrusionTypeToPointExtrusion();
+  extrude->SetExtrusionTypeToNormalExtrusion();
   extrude->SetVector(0, 0, 10);
   extrude->SetScaleFactor(0.5);
-
-  vtkNew<vtkTriangleFilter> triangleFilter;//
+  
+  //required for non-convex shape
+  vtkNew<vtkTriangleFilter> triangleFilter;
   triangleFilter->SetInputConnection(extrude->GetOutputPort());
 
-  vtkNew<vtkDataSetMapper> mapper;
+  /*if we would like to link extrusion to mapper,
+  * we have to use vtkDataSetMapper instead of vtkPolyDataMapper
+  */ 
+  vtkNew<vtkDataSetMapper> mapper; 
   mapper->SetInputConnection(triangleFilter->GetOutputPort());
+
+  /*Functions for triangulate the polydata without extrusion*/
+  // vtkNew<vtkTriangleFilter> triangleFilter;
+  // triangleFilter->SetInputData(polyData);
+  // triangleFilter->Update();
+  // vtkNew<vtkPolyDataMapper> mapper;
+  // mapper->SetInputData(triangleFilter->GetOutput());
 
   vtkNew<vtkActor> actor;
   actor->SetMapper(mapper);
+  //actor->GetProperty()->SetRepresentationToWireframe();
   actor->GetProperty()->SetColor(colors->GetColor3d("PaleGoldenrod").GetData());
 
-  renderer->SetBackground(colors->GetColor3d("SteelBlue").GetData());
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("DoubleTCell");
+  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
 
   renderWindow->AddRenderer(renderer);
-  renderWindow->SetWindowName("LinearExtrusion");
-
   renderer->AddActor(actor);
 
   renderer->ResetCamera();
