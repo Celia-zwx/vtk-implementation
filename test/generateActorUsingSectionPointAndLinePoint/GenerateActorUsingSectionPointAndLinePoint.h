78600bdb90d864023a894f7f442d8a3c0290ef05
unknown <wzhang488@gatech.edu>
2024-02-06 09:25:55 +0800

    report for animation

--- test/generateActorUsingSectionPointAndLinePoint/GenerateActorUsingSectionPointAndLinePoint.h
+++
@@ -1,4 +0,0 @@
-#include <vtkActor.h>
-#include <vtkSmartPointer.h>
-vtkSmartPointer<vtkActor> generateActorUsingSectionPointAndLinePoint(std::vector<std::array<double, 3>> SectionPoint,
-                                                                     std::vector<std::array<double, 3>> LinePoint);
